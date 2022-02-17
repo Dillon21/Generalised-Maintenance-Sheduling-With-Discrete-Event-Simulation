5ef008c0bec729f7f2cb2a70fc51463e91ed7774
Dillon21 <dillon9821@gmail.com>
2022-02-17 03:52:16 +0000

    break with successful repair

--- asset sim.py
+++ asset sim.py
@@ -7,14 +7,15 @@ Created on Tue Feb 15 19:28:24 2022
 
 
 class asset(object):
-    def __init__(self, env, name, age, break_Chance):
+    def __init__(self, env, name, age):
          self.env = env
          # Start the run process everytime an instance is created.
          self.action = env.process(self.run())
          self.name = name
          self.age = age
-         self.break_Chance = break_Chance
+         self.break_Chance = 0
          self.broken = False
+         self.repairTime = 0
         
     def run(self):
         #initialse brake chance according to age
@@ -30,8 +31,11 @@ class asset(object):
             #track hours through day process
             Day.workDay()
             # wait for working day to end
+            
             yield self.env.process(self.work())
+        
             print("asset is operating till hour %d" % self.env.now)
+            
             
             #pass a nighttime into environment                       
             Day.night()
@@ -42,6 +46,18 @@ class asset(object):
         # 8 hours working day
         hours = 8
         while hours > 0:
+            
+            #if asset is broken stop working to repair
+            while self.broken == True:
+                yield self.env.timeout(1)
+                hours -= 1
+                self.repairTime -= 1
+                print("repair hours = %d " % self.getRepairTime())
+                if self.repairTime == 0:
+                    self.broken = False
+                    self.maintain()
+            
+            #normal operation    
             yield self.env.timeout(1)
             self.deteriorate(self)
             print(hours)
@@ -57,19 +73,15 @@ class asset(object):
     #add code that will randomly break the asset    
     @staticmethod
     def fail(self):
-        import random
-        import time
-        random.seed(time.process_time())
-        value = round(random.uniform(0,1), 4)
+        value = self.getRandom()
         print("value = ",value)
-        print("chance = ", self.break_Chance)
-        #check if asset is broken
-        if self.broken == True:
-            print("repair time")
+
         #break if asset is over threshold    
-        elif value >= (1 - self.break_Chance):
+        if value >= (1 - self.break_Chance):
             self.broken = True
             print("broken")
+            self.repairTime = 4
+            
         elif value < (1-self.break_Chance):
             self.broken = False
             print("working")
@@ -84,11 +96,21 @@ class asset(object):
     def maintain(self):
         self.break_Chance = (self.age / 100)
         
-        
+    def getRepairTime(self):
+        return self.repairTime
+    
+    def getRandom(self):
+        import random
+        import time
+        time.sleep(1)
+        random.seed(time.localtime())
+        value = round(random.uniform(0,1), 4)
+        return value
         
 import simpy
 env = simpy.Environment()
-asset1 = asset(env,"ford", 5, 0)
+asset1 = asset(env,"ford", 5)
+
 env.run(until=49)
 
 
